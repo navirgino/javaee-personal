3b0a46a872a480acc6c1845dfe255142926a4fae
Nico Virgino <nicovirgino@gmail.com>
2020-01-15 08:49:27 -0600

    finished sw db

--- src/db/sw_db.sql
+++ src/db/sw_db.sql
@@ -1,13 +1,20 @@
-drop table if exists create sw_db;
+# drop table if exists create sw_db;
 create database sw_db;
 use sw_db;
 
 create table monsters (
     id int not null auto_increment primary key,
-    name varchar(50),
-    skill_one varchar(30),
-    skill_two varchar(30),
-    skill_three varchar(30),
-
+    name varchar(225),
+    element varchar(10) not null,
+    health_point int(11) not null,
+    attack int(11) not null,
+    defense int(11) not null,
+    speed int(11) not null,
+    critical_rate int(11) not null,
+    critical_damage int(11) not null,
+    resistance int(11) not null,
+    star_grade int(11) not null,
+    accuracy int(11) not null,
+    is_awaken tinyint(1) not null
 
 );
