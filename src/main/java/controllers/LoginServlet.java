225a176a4585deb6c9cd63dc958a9b8ff2057928
Nico Virgino <nicovirgino@gmail.com>
2020-01-16 21:05:03 -0600

    login doesn't work will fix later

--- src/main/java/controllers/LoginServlet.java
+++ src/main/java/controllers/LoginServlet.java
@@ -5,12 +5,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
-    protected void  doGet(HttpServletRequest req, HttpServletResponse res)
-    {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        if (req.getSession().getAttribute("user") != null){
+            res.sendRedirect("/profile");
+            return;
+        }
+        req.getRequestDispatcher("/WEB-INF/login.jsp");
+    }
+    protected void  doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
 
+        if(username.equalsIgnoreCase("banana") && password.equals("pw"))
+        {
+            res.sendRedirect("/profile");
+        }
     }
 
 }
