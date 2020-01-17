a8aff8be563d6f1cacae6e634169c47261e207b1
Nico Virgino <nicovirgino@gmail.com>
2020-01-17 06:34:35 -0600

    finishing up login

--- src/main/java/controllers/LoginServlet.java
+++ src/main/java/controllers/LoginServlet.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 @WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+
         if (req.getSession().getAttribute("user") != null){
             res.sendRedirect("/profile");
             return;
@@ -20,11 +21,19 @@ public class LoginServlet extends HttpServlet {
     protected void  doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
+//        if (user == null) {
+//            res.sendRedirect("/login");
+//            return;
+//        }
 
         if(username.equalsIgnoreCase("banana") && password.equals("pw"))
         {
             res.sendRedirect("/profile");
+            return;
         }
+
+        //create a valid attempt for log in
+        boolean validAttempt = true;
     }
 
 }
