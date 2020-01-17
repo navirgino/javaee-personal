ddbf52438347b5a510d1a003ecea22609d46b711
Nico Virgino <nicovirgino@gmail.com>
2020-01-16 20:25:16 -0600

    login servlet start

--- src/main/java/controllers/LoginServlet.java
+++ src/main/java/controllers/LoginServlet.java
@@ -1,4 +1,16 @@
 package controllers;
 
-public class LoginServlet {
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void  doGet(HttpServletRequest req, HttpServletResponse res)
+    {
+
+    }
+
 }
