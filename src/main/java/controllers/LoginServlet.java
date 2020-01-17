2def31b0f56857f450cfd316b22f22e090069519
Nico Virgino <nicovirgino@gmail.com>
2020-01-16 21:08:46 -0600

    fixed login servlet......... silly mistakes

--- src/main/java/controllers/LoginServlet.java
+++ src/main/java/controllers/LoginServlet.java
@@ -1,6 +1,7 @@
 package controllers;
 
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,12 +10,12 @@ import java.io.IOException;
 
 @WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         if (req.getSession().getAttribute("user") != null){
             res.sendRedirect("/profile");
             return;
         }
-        req.getRequestDispatcher("/WEB-INF/login.jsp");
+        req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, res);
     }
     protected void  doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
         String username = req.getParameter("username");
